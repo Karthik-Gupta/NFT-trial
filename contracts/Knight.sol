daa35f62c35e6b0917c7e233c426eab7512fa279
Karthik Gupta <karthikts.gupta@gmail.com>
2021-09-25 21:28:23 +0530

    Update Knight.sol

--- contracts/Knight.sol
+++ contracts/Knight.sol
@@ -8,12 +8,12 @@ contract Knights is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenId;
 
+    uint public knightsCount;
+
     struct Knight {
         uint tokenId;
         string tokenUri;
-        address mintedBy;
-        address currentOwner;
-        address previousOwner;
+        address owner;
         uint price;
         uint numberOfTransfers;
         bool forSale;
@@ -22,6 +22,11 @@ contract Knights is ERC721URIStorage {
     mapping(uint => Knight) public knightCollection;
     /* check if token URI exists */
     mapping(string => bool) public tokenURIExists;
+
+    event KnightGenerated(uint tokenId, string tokenUri, address mintedBy, uint price, bool forSale);
+    event KnightBought(uint tokenId, string tokenUri, address currentOwner, address previousOwner, uint price, uint transfers);
+    event KnightForSale(uint tokenId, string tokenUri, address owner, bool forSale);
+
 
     constructor() ERC721("Knight Collectibles ", "KNC") {
     }
@@ -37,17 +42,19 @@ contract Knights is ERC721URIStorage {
         return _tokenId.current();
     }
 
-    function mintKnight(string memory _tokenUri, uint _price, bool _putOnSale) public returns (uint) {
+    function mintKnight(string memory _tokenUri, uint _price, bool _putOnSale) external returns (uint) {
         require(!tokenURIExists[_tokenUri], "Knight: URI already minted");
         uint tokenCounterId = incrementAndGet();
         _safeMint(msg.sender, tokenCounterId);
         _setTokenURI(tokenCounterId, _tokenUri);
         tokenURIExists[_tokenUri] = true;
-        knightCollection[tokenCounterId] = Knight(tokenCounterId, _tokenUri, msg.sender, msg.sender, address(0), _price, 0, _putOnSale);
+        knightsCount = tokenCounterId;
+        knightCollection[tokenCounterId] = Knight(tokenCounterId, _tokenUri, msg.sender, _price, 0, _putOnSale);
+        emit KnightGenerated(tokenCounterId, _tokenUri, msg.sender, _price, _putOnSale);
         return tokenCounterId;
     }
 
-    function buyKnight(uint _knightTokenId) public payable {
+    function buyKnight(uint _knightTokenId) external payable {
         Knight memory kInstance = knightCollection[_knightTokenId];
         require(msg.value >= kInstance.price, "Knight: Insufficient funds");
         require(kInstance.forSale, "Knight: Not for sale");
@@ -55,16 +62,18 @@ contract Knights is ERC721URIStorage {
         require(knightCurrentOwner == kInstance.currentOwner, "Knight: Knight ownership dispute");
         super.safeTransferFrom(knightCurrentOwner, msg.sender, _knightTokenId);
         payable(knightCurrentOwner).transfer(msg.value);
-        kInstance.previousOwner = knightCurrentOwner;
-        kInstance.currentOwner = msg.sender;
+        kInstance.owner = msg.sender;
         kInstance.numberOfTransfers += 1;
         kInstance.price = msg.value;
+        emit KnightBought(_knightTokenId, kInstance.tokenUri, msg.sender, knightCurrentOwner, kInstance.price, kInstance.numberOfTransfers);
         knightCollection[_knightTokenId] = kInstance;
     }
     
-    function setForSale(uint _knightTokenId, bool _forSale) public knightOwnerPrivilege(_knightTokenId) {
-        Knight storage knightInst = knightCollection[_knightTokenId];
+    function setForSale(uint _knightTokenId, bool _forSale) external knightOwnerPrivilege(_knightTokenId) {
+        Knight memory knightInst = knightCollection[_knightTokenId];
         knightInst.forSale = _forSale;
+        emit KnightForSale(_knightTokenId, knightInst.tokenUri, knightInst.owner, _forSale);
+        knightCollection[_knightTokenId] = knightInst;
     }
 
 }
